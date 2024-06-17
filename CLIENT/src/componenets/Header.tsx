55099e55973d3a3fd5561bce17102013e0b729fb
pawan <pawanwarhade36@gmail.com.com>
2024-06-17 22:58:53 +0530

    nskxxoaiasaoisiasuaojaoaiaxia aiaauia kaahaja kaja

--- CLIENT/src/componenets/Header.tsx
+++ CLIENT/src/componenets/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 
 
@@ -7,7 +8,17 @@ export const Header = () => {
         <div className="container mx-auto 
         flex justify-between"></div>
         <span className="text-3xl text-white font-bold tracking-tight">
+            <Link to="/"> Holiday.com</Link>
+        </span>
+        <span className="flex space-x-2">
             
+            <Link to="/sign-in 
+            className="flex items-center
+             text-blue-600 py-3 font-bold
+              hover:bg-gray-100
+               hover:text-gray-500 ">
+            
+            </Link>
         </span>
     </div>
   )
