e1a94c522eb4242e923434c4a541abe17957fc93
pawan <pawanwarhade36@gmail.com.com>
2024-06-18 22:47:54 +0530

    nothing

--- CLIENT/src/layouts/Layout.tsx
+++ CLIENT/src/layouts/Layout.tsx
@@ -1,13 +1,18 @@
 import { Header } from "../componenets/Header"
 import { Hero } from "../componenets/Hero"
+import { Footer } from "../componenets/Footer"
 
+ interface Props{
+  children: React.ReactNode;
+ }
 
-
-const Layout = () =>{
+const Layout = ({children}: Props) =>{
     return(
       <div className=" flex flex-col min-h-screen">
         <Header/>
         <Hero/>
+        <div className="container mx-auto py-10 flex-1 ">{children}</div>
+        <Footer />
       </div>
     )
 }
