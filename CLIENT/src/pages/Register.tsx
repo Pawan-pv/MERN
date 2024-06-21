1d8b624e43366962577d37ce5cc8fda1dc75a20f
pawan <pawanwarhade36@gmail.com.com>
2024-06-21 15:43:52 +0530

    change   register to create account

--- CLIENT/src/pages/Register.tsx
+++ CLIENT/src/pages/Register.tsx
@@ -81,7 +81,7 @@ const Register = () => {
         </div>
       </div>
       <button type="submit" className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
-        Register
+        Create Account
       </button>
     </form>
   );
