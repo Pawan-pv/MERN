12e113417fe7a987d3aadee0b8e2fa758796a5f9
pawan <pawanwarhade36@gmail.com.com>
2024-06-21 16:40:43 +0530

    dd

--- CLIENT/src/pages/Register.tsx
+++ CLIENT/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client"
 
-type RegisterFormData = {
+export type RegisterFormData = {
   firstName: string;
   lastName: string;
   email: string;
@@ -16,7 +18,18 @@ const Register = () => {
     formState: { errors },
   } = useForm<RegisterFormData>();
 
+  const mutation = useMutation(apiClient.register, {
+    onSuccess: ()=>{
+      console.log("registeration succesful!")
+    },
+    onError: (error: Error) =>{
+      console.log(error.message)
+    }
+  });
+  
+
   const onSubmit = (data: RegisterFormData) => {
+    mutation.mutate(data);
     console.log(data);
   };
 
